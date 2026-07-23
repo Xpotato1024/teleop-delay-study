738d6923bae073c682b4f7db6758feddb276c15c
miyut <widwmitsui@gmail.com>
2026-07-23 14:27:00 +0900

    fix: Issue #9図のレイアウトと文字色を改善

--- src/+teleopdelay/+analysis/style_figure.m
+++ src/+teleopdelay/+analysis/style_figure.m
@@ -6,6 +6,10 @@ fontObjects = findall(fig, "-property", "FontColor");
 for index = 1:numel(fontObjects)
     fontObjects(index).FontColor = [0.05, 0.05, 0.05];
 end
+textObjects = findall(fig, "Type", "text");
+for index = 1:numel(textObjects)
+    textObjects(index).Color = [0.05, 0.05, 0.05];
+end
 axesObjects = findall(fig, "Type", "axes");
 for index = 1:numel(axesObjects)
     set(axesObjects(index), "Color", "white", "XColor", [0.05, 0.05, 0.05], ...
